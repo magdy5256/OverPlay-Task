4882724cf8999616f91055f3e040e655c537b086
Magdy Khaled <magdy.khaled52156@gmail.com>
2022-05-28 19:07:04 +0200

    Using gyroscope events

--- OverPlay task/ViewController.swift
+++ OverPlay task/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import AVFoundation
 import CoreLocation
+import CoreMotion
+import MediaPlayer
 
 class ViewController: UIViewController {
     
@@ -22,14 +24,32 @@ class ViewController: UIViewController {
     let videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
     var currentLocation: CLLocation!
     var oldLocation: CLLocation!
+    let motion = CMMotionManager()
+    var timer: Timer!
+    var oldX: Double!
+    var oldZ: Double!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.becomeFirstResponder()
         initializeView()
-        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startGyros()
+        if isLocationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    deinit {
+        playerLooper = nil
+        locationManager.stopUpdatingLocation()
+        stopGyros()
+    }
+    
+    
     func initializeView() {
         locationManager = CLLocationManager()
         locationManager.delegate = self
@@ -41,49 +61,107 @@ class ViewController: UIViewController {
     override func becomeFirstResponder() -> Bool {
         return true
     }
-
+    
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?){
-        if motion == . {
+        if motion == .motionShake {
             player.pause()
         }
     }
+
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // Check for auth
-        if isLocationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-        } else {
-            locationManager.requestWhenInUseAuthorization()
+    func startGyros() {
+        if motion.isGyroAvailable {
+            self.motion.gyroUpdateInterval = 10.0 / 60.0
+            self.motion.startGyroUpdates()
+            self.timer = Timer(fire: Date(), interval: (10.0/60.0),
+                               repeats: true, block: { (timer) in
+                // Get the gyro data.
+                if let data = self.motion.gyroData {
+                    if self.oldX == nil {
+                        self.oldX = self.motion.gyroData?.rotationRate.x
+                    }
+                    if self.oldZ == nil {
+                        self.oldZ = self.motion.gyroData?.rotationRate.z
+                    }
+                    self.setVolume(newX: data.rotationRate.x )
+                    self.setVideoPosition(newZ:data.rotationRate.z )
+                }
+            })
+            RunLoop.current.add(self.timer!, forMode: .default)
         }
-        // Do other stuff
     }
-    deinit {
-        playerLooper = nil
-        locationManager.stopUpdatingLocation()
+    
+    func setVolume(newX:Double) {
+        let x = newX
+        if x > self.oldX + 0.05 {
+            let volume = self.player.volume
+            if volume != 1 {
+                self.player.volume = volume + 0.0625
+            }
+        } else if x < self.oldX + 0.05 {
+            let volume = self.player.volume
+            if volume != 0 {
+                self.player.volume = volume - 0.0625
+            }
+        }
+    }
+    
+    func setVideoPosition(newZ:Double) {
+        let z = newZ
+        if z > self.oldZ + 0.05 {
+            guard let duration = player.currentItem?.duration else { return }
+            let currentTime = CMTimeGetSeconds(player.currentTime())
+            let newTime = currentTime + 5.0
+            if newTime < (CMTimeGetSeconds(duration) - 5.0) {
+                let time: CMTime = CMTimeMake(value: Int64(newTime*1000), timescale: 1000)
+                player.seek(to: time)
+            }
+            
+        } else if z < self.oldZ + 0.05 {
+            if player.currentTime().value != 0 {
+                let currentTime = CMTimeGetSeconds(player.currentTime())
+                var newTime = currentTime - 5.0
+                
+                if newTime < 0 {
+                    newTime = 0
+                }
+                let time: CMTime = CMTimeMake(value: Int64(newTime * 1000 ), timescale: 1000)
+                player.seek(to: time)
+            }
+        }
+    }
+    
+    func stopGyros() {
+        if self.timer != nil {
+            self.timer?.invalidate()
+            self.timer = nil
+            
+            self.motion.stopGyroUpdates()
+        }
     }
     
     func playVideo() {
         videoView.layer.sublayers = nil
         if let pathURL = URL(string: self.videoURL) {
-            let duration = Int64( ( (Float64(CMTimeGetSeconds(AVAsset(url: pathURL).duration)) *  10.0) - 1) / 10.0 )
+            let duration = Int64(((Float64(CMTimeGetSeconds(AVAsset(url: pathURL).duration)) *  10.0) - 1) / 10.0 )
             player = AVQueuePlayer()
             playerLayer = AVPlayerLayer(player: player)
             playerItem = AVPlayerItem(url: pathURL)
             playerLooper = AVPlayerLooper(player: player, templateItem: playerItem,
                                           timeRange: CMTimeRange(start:  CMTime.zero, end: CMTimeMake(value: duration, timescale: 1)) )
-            playerLayer.videoGravity = .resizeAspectFill
+            playerLayer.videoGravity = .resizeAspect
+            try! AVAudioSession.sharedInstance().setCategory(.playback)
             playerLayer.frame = videoView.layer.bounds
             videoView.layer.insertSublayer(playerLayer, at: 1)
             player.play()
-            
-            
         }
     }
+    
     func restartVideo() {
         self.player.seek(to: CMTime.zero)
         self.player.play()
     }
+    
     func isLocationServicesEnabled() -> Bool {
         if CLLocationManager.locationServicesEnabled() {
             switch(CLLocationManager.authorizationStatus()) {
@@ -115,3 +193,4 @@ extension ViewController : CLLocationManagerDelegate {
         }
     }
 }
+
